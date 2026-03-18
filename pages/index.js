15378ca83c8264861912b8bbdfde38d45d2fac60
KenChen <aken112981@gmail.com>
2026-03-18 22:40:12 +0800

    Add animated glowing and shimmer backgrounds to Main layout and theme

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 // import NextLink from 'next/link'
-import dynamic from 'next/dynamic'
 import {
   // Link,
   Container,
@@ -36,309 +35,302 @@ const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 })
 
-const HomeThreeBg = dynamic(() => import('../components/home-three-bg'), {
-  ssr: false
-})
-
 const Home = () => (
   <Layout>
-    <HomeThreeBg />
-    <Box position="relative" zIndex={1}>
-      <Container>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        嗨, 欢迎. 这是我的一些个人作品展示
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            陈 肯
+          </Heading>
+          <p>游戏开发工程师</p>
+        </Box>
         <Box
-          borderRadius="lg"
-          mb={6}
-          p={3}
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
           textAlign="center"
-          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-          css={{ backdropFilter: 'blur(10px)' }}
         >
-          嗨, 欢迎. 这是我的一些个人作品展示
-        </Box>
-
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1}>
-            <Heading as="h2" variant="page-title">
-              陈 肯
-            </Heading>
-            <p>游戏开发工程师</p>
-          </Box>
           <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
-            textAlign="center"
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
           >
-            <Box
-              borderColor="whiteAlpha.800"
-              borderWidth={2}
-              borderStyle="solid"
-              w="100px"
-              h="100px"
-              display="inline-block"
+            <ProfileImage
+              src={withBasePath('/images/takuya.jpg')}
+              alt="Profile image"
               borderRadius="full"
-              overflow="hidden"
-            >
-              <ProfileImage
-                src={withBasePath('/images/takuya.jpg')}
-                alt="Profile image"
-                borderRadius="full"
-                width="100%"
-                height="100%"
-              />
-            </Box>
+              width="100%"
+              height="100%"
+            />
           </Box>
         </Box>
- 
-        {/* <Section delay={0.1}>
-          <Heading as="h3" variant="section-title">
-            Work
-          </Heading>
-          <Paragraph>
+      </Box>
 
-          </Paragraph>
-          <Box align="center" my={4}>
-            <NextLink href="/works" passHref scroll={false}>
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                My portfolio
+      {/* <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section> */}
+
+      {/* <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1984</BioYear>
+          Born in Osaka (大阪), Japan.
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Completed the Master&apos;s Program in the Graduate School of
+          Information Science at Nara Institute of Science and Technology
+          (奈良先端科学技術大学院大学情報科学研究科修士課程)
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Worked at Yahoo! Japan (ヤフー株式会社入社)
+        </BioSection>
+        <BioSection>
+          <BioYear>2012 to present</BioYear>
+          Working as a freelancer
+        </BioSection>
+      </Section> */}
+
+      {/* <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Art, Music,{' '}
+          <Link href="https://illust.odoruinu.net/" ">
+            Drawing
+          </Link>
+          , Playing Drums,{' '}
+          <Link href="https://500px.com/p/mikeKen" ">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </Paragraph>
+      </Section> */}
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          2D 游戏
+        </Heading>
+        {/* <List>
+          <ListItem>
+            <Link href="https://github.com/mikeKen" ">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @mikeKen
               </Button>
-            </NextLink>
-          </Box>
-        </Section> */}
-
-        {/* <Section delay={0.2}>
-          <Heading as="h3" variant="section-title">
-            Bio
-          </Heading>
-          <BioSection>
-            <BioYear>1984</BioYear>
-            Born in Osaka (大阪), Japan.
-          </BioSection>
-          <BioSection>
-            <BioYear>2010</BioYear>
-            Completed the Master&apos;s Program in the Graduate School of
-            Information Science at Nara Institute of Science and Technology
-            (奈良先端科学技術大学院大学情報科学研究科修士課程)
-          </BioSection>
-          <BioSection>
-            <BioYear>2010</BioYear>
-            Worked at Yahoo! Japan (ヤフー株式会社入社)
-          </BioSection>
-          <BioSection>
-            <BioYear>2012 to present</BioYear>
-            Working as a freelancer
-          </BioSection>
-        </Section> */}
-
-        {/* <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            I ♥
-          </Heading>
-          <Paragraph>
-            Art, Music,{' '}
-            <Link href="https://illust.odoruinu.net/" ">
-              Drawing
             </Link>
-            , Playing Drums,{' '}
-            <Link href="https://500px.com/p/mikeKen" ">
-              Photography
-            </Link>
-            , Leica, Machine Learning
-          </Paragraph>
-        </Section> */}
-
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            2D 游戏
-          </Heading>
-          {/* <List>
-            <ListItem>
-              <Link href="https://github.com/mikeKen" ">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<IoLogoGithub />}
-                >
-                  @mikeKen
-                </Button>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://twitter.com/inkdrop_app" ">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<IoLogoTwitter />}
-                >
-                  @inkdrop_app (English)
-                </Button>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://twitter.com/mikeKen" ">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<IoLogoTwitter />}
-                >
-                  @mikeKen (日本語)
-                </Button>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://instagram.com/mikeKen" ">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<IoLogoInstagram />}
-                >
-                  @mikeKen
-                </Button>
-              </Link>
-            </ListItem>
-          </List> */}
-
-          <SimpleGrid columns={[1, 2, 2]} gap={6}>
-            <GridItem
-              href="http://oss.mikeken.online/brainwash/web-mobile/index.html"
-              title="脑洞太大了吧"
-              thumbnail={thumbBrainWash}
-            >
-              <Box color="gray.500" fontSize="sm">
-                益智解谜类
-              </Box>
-            </GridItem>
-            <GridItem
-              href="http://oss.mikeken.online/popstar/web-mobile/index.html"
-              title="天天玩消星星"
-              thumbnail={thumbStar}
-            >
-              <Box color="gray.500" fontSize="sm">
-                消除类
-              </Box>
-            </GridItem>
-
-          </SimpleGrid>
-
-          {/* <Box align="center" my={4}>
-            <NextLink href="/posts" passHref scroll={false}>
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                Popular posts
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/inkdrop_app" ">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @inkdrop_app (English)
               </Button>
-            </NextLink>
-          </Box> */}
-        </Section>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/mikeKen" ">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @mikeKen (日本語)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/mikeKen" ">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @mikeKen
+              </Button>
+            </Link>
+          </ListItem>
+        </List> */}
 
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            3D 游戏
-          </Heading>
-          <SimpleGrid columns={[1, 2, 2]} gap={6}>
-            <GridItem
-              href="http://oss.mikeken.online/crashload/web-mobile/index.html"
-              title="紧急迫降"
-              thumbnail={thumbCrashLoad}
-            >
-              <Box color="gray.500" fontSize="sm">
-                飞行类
-              </Box>
-            </GridItem>
-            <GridItem
-              href="http://oss.mikeken.online/dippuzzle/web-mobile/index.html"
-              title="蘸色小能手"
-              thumbnail={thumbDipPuzzle}
-            >
-              <Box color="gray.500" fontSize="sm">
-                休闲益智类
-              </Box>
-            </GridItem>
-          </SimpleGrid>
-        </Section>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="http://oss.mikeken.online/brainwash/web-mobile/index.html"
+            title="脑洞太大了吧"
+            thumbnail={thumbBrainWash}
+          >
+            <Box color="gray.500" fontSize="sm">
+              益智解谜类
+            </Box>
+          </GridItem>
+          <GridItem
+            href="http://oss.mikeken.online/popstar/web-mobile/index.html"
+            title="天天玩消星星"
+            thumbnail={thumbStar}
+          >
+            <Box color="gray.500" fontSize="sm">
+              消除类
+            </Box>
+          </GridItem>
 
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            渲染
-          </Heading>
-          <SimpleGrid columns={[1, 2, 2]} gap={6}>
-            <GridItem
-              href="http://oss.mikeken.online/shader/web-desktop/index.html"
-              title="2D shader(电脑打开)"
-              thumbnail={thumb2dshader}
-            >
-              <Box color="gray.500" fontSize="sm">
-                常见的一些 2D shader
-              </Box>
-            </GridItem>
-            <GridItem
-              href="http://oss.mikeken.online/blinn-phong/web-desktop/index.html"
-              title="3D shader(电脑打开)"
-              thumbnail={thumb3dshader}
-            >
-              <Box color="gray.500" fontSize="sm">
-                法线着色, blinn-phong模型, 纹理映射
-              </Box>
-            </GridItem>
+        </SimpleGrid>
 
-          </SimpleGrid>
-        </Section>
+        {/* <Box align="center" my={4}>
+          <NextLink href="/posts" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box> */}
+      </Section>
 
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            其他
-          </Heading>
-          <SimpleGrid columns={[1, 2, 2]} gap={6}>
-            <GridItem
-              href="http://oss.mikeken.online/quadTree/web-mobile/index.html"
-              title="四叉树"
-              thumbnail={thumbQuadTree}
-            >
-              <Box color="gray.500" fontSize="sm">
-                用于优化碰撞检测的一种数据结构
-              </Box> </GridItem>
-            <GridItem
-              href="http://oss.mikeken.online/convexHull/web-mobile/index.html"
-              title="凸包"
-              thumbnail={thumbConvexHull}
-            >            <Box color="gray.500" fontSize="sm">
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          3D 游戏
+        </Heading>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="http://oss.mikeken.online/crashload/web-mobile/index.html"
+            title="紧急迫降"
+            thumbnail={thumbCrashLoad}
+          >
+            <Box color="gray.500" fontSize="sm">
+              飞行类
+            </Box>
+          </GridItem>
+          <GridItem
+            href="http://oss.mikeken.online/dippuzzle/web-mobile/index.html"
+            title="蘸色小能手"
+            thumbnail={thumbDipPuzzle}
+          >
+            <Box color="gray.500" fontSize="sm">
+              休闲益智类
+            </Box>
+          </GridItem>
+        </SimpleGrid>
+      </Section>
 
-                用于获取一堆点组成的的凸多边形 </Box>
-            </GridItem>
-            <GridItem
-              href="http://oss.mikeken.online/Ramer-Douglas-Peucker/web-mobile/index.html"
-              title="Ramer-Douglas-Peucker"
-              thumbnail={thumbRamerDouglasPeucker}
-            >
-              <Box color="gray.500" fontSize="sm">
-                在一定的阈值范围内简化曲线上的点
-              </Box>
-            </GridItem>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          渲染
+        </Heading>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="http://oss.mikeken.online/shader/web-desktop/index.html"
+            title="2D shader(电脑打开)"
+            thumbnail={thumb2dshader}
+          >
+            <Box color="gray.500" fontSize="sm">
+              常见的一些 2D shader
+            </Box>
+          </GridItem>
+          <GridItem
+            href="http://oss.mikeken.online/blinn-phong/web-desktop/index.html"
+            title="3D shader(电脑打开)"
+            thumbnail={thumb3dshader}
+          >
+            <Box color="gray.500" fontSize="sm">
+              法线着色, blinn-phong模型, 纹理映射
+            </Box>
+          </GridItem>
 
-            <GridItem
-              href="http://oss.mikeken.online/threejs/slicing/index.html"
-              title="3D 套索工具"
-              thumbnail={slicing}
-            >
-            </GridItem>
-          </SimpleGrid>
-        </Section>
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          其他
+        </Heading>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="http://oss.mikeken.online/quadTree/web-mobile/index.html"
+            title="四叉树"
+            thumbnail={thumbQuadTree}
+          >
+            <Box color="gray.500" fontSize="sm">
+              用于优化碰撞检测的一种数据结构
+            </Box> </GridItem>
+          <GridItem
+            href="http://oss.mikeken.online/convexHull/web-mobile/index.html"
+            title="凸包"
+            thumbnail={thumbConvexHull}
+          >            <Box color="gray.500" fontSize="sm">
+
+              用于获取一堆点组成的的凸多边形 </Box>
+          </GridItem>
+          <GridItem
+            href="http://oss.mikeken.online/Ramer-Douglas-Peucker/web-mobile/index.html"
+            title="Ramer-Douglas-Peucker"
+            thumbnail={thumbRamerDouglasPeucker}
+          >
+            <Box color="gray.500" fontSize="sm">
+              在一定的阈值范围内简化曲线上的点
+            </Box>
+          </GridItem>
+
+          <GridItem
+            href="http://oss.mikeken.online/threejs/slicing/index.html"
+            title="3D 套索工具"
+            thumbnail={slicing}
+          >
+          </GridItem>
+        </SimpleGrid>
+      </Section>
 
 
 
 
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            学习笔记
-          </Heading>
-          <NotesAccordion noteGroups={noteGroups}
-          ></NotesAccordion>
-          <SimpleGrid columns={[1]} gap={6}>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          学习笔记
+        </Heading>
+        <NotesAccordion noteGroups={noteGroups}
+        ></NotesAccordion>
+        <SimpleGrid columns={[1]} gap={6}>
 
 
 
-          </SimpleGrid>
-        </Section>
-      </Container>
-    </Box>
+        </SimpleGrid>
+      </Section>
+    </Container>
   </Layout >
 )
 
